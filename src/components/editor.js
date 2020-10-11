8b44c43140b845e4399389c86e405df44be91a26
Meshach Adoe <madoe@ucsd.edu>
2020-10-11 09:49:41 +0700

    Fix buttons and code editor

--- src/components/editor.js
+++ src/components/editor.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Editor() {
+	const [code, setCode] = useState('');
+
 	return (
 		<div className='editor'>
-			{/* <h2>tartle.code</h2> */}
+			<h2>tartle.code</h2>
 			<div className='editor-code'>
-				<pre className='code-number'></pre>
-				<pre contentEditable='true' className='code-text'></pre>
-
-				{/* {'\n'} */}
-				{/* <mark>1{'\n'}</mark>2{'\n'}3{'\n'}4{'\n'}5{'\n'} */}
-
-				{/* {'\n'} */}
-				{/* <mark>move(20){'\n'}</mark> */}
-				{/* turn(180){'\n'}penDown(){'\n'}move(10){'\n'}turn(90){'\n'} */}
+				<pre className='code-number'>
+					<mark>1{'\n'}</mark>2{'\n'}3{'\n'}4{'\n'}5{'\n'}
+				</pre>
+				<pre contentEditable='true' className='code-text'>
+					<mark>move(20){'\n'}</mark>
+					turn(180){'\n'}penDown(){'\n'}move(10)
+					{'\n'}turn(90){'\n'}
+				</pre>
 			</div>
 			<button>Run</button>
 		</div>
